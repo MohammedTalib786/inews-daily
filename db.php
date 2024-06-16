c84d0fc5e87b5dfefa5259e8ee2989cb31369cab
Shaikh Mohammed Talib <shaikhtalib2705@gmail.com>
2024-06-16 15:30:08 +0530

    Updated

--- db.php
+++ db.php
@@ -3,15 +3,15 @@
 // ONLINE
 // data transferred to another file for security concerns (onldetign)
 
-
 // LOCAL
 $db_server = "localhost";
 $db_user = "root";
 $db_pass = "";
 $db_name = "inews-app";
-$port = 4306;
+// $port = 4306;
 
-$con = mysqli_connect($db_server, $db_user, $db_pass, $db_name, $port);
+// $con = mysqli_connect($db_server, $db_user, $db_pass, $db_name, $port);
+$con = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
 if(!$con){
     echo "NOT Connected!";
